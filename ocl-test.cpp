ab48899567d1b5d456c177a0398a64f119672c52
Davide Madrisan <davide.madrisan@gmail.com>
2015-05-02 18:33:02 +0200

    ocl-test: only display the working OpenCL platforms
    Signed-off-by: Davide Madrisan <davide.madrisan@gmail.com>

--- ocl-test.cpp
+++ ocl-test.cpp
@@ -5,17 +5,38 @@
 
 int main () {
     try {
-        std::vector<cl::Platform> platformList;
-        cl::Platform::get (&platformList);
-        if (platformList.size () == 0) {
+        std::vector<cl::Platform> platforms;
+        cl::Platform::get (&platforms);
+        if (platforms.size () == 0) {
             std::cout << "No OpenCL platforms detected" << std::endl;
             return -1;
         }
 
-        for (auto p : platformList) {
-            std::string platformVendor;
-            p.getInfo (CL_PLATFORM_NAME, &platformVendor);
-            std::cout << platformVendor << std::endl;
+        unsigned int platformID (0);
+
+        for (auto p : platforms) {
+            std::string platformVendor, platformName;
+            p.getInfo (CL_PLATFORM_VENDOR, &platformVendor);
+            p.getInfo (CL_PLATFORM_NAME, &platformName);
+
+            cl_context_properties properties[] = {
+                CL_CONTEXT_PLATFORM,
+                (cl_context_properties)(platforms[platformID])(),
+                0
+            };
+
+            platformID++;
+
+            try {
+                cl::Context context (CL_DEVICE_TYPE_ALL, properties);
+                std::vector<cl::Device> devices =
+                    context.getInfo<CL_CONTEXT_DEVICES> ();
+            } catch (cl::Error err) {
+                continue;
+            }
+
+            std::cout << platformVendor << " (" << platformName << ")"
+                      << std::endl;
         }
     } catch (cl::Error err) {
         std::cerr << "ERROR: " << err.what () << "(" << err.err () << ")"
